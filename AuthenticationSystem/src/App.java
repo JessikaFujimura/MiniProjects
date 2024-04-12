5dc205cc9447d49dd96204ae68f2eced19a2b8e0
jessika <jessikafujimura@gmail.com>
2024-04-11 23:01:16 -0300

    feat: add method logIn in AuthenticationSystem

--- AuthenticationSystem/src/App.java
+++ AuthenticationSystem/src/App.java
@@ -7,12 +7,32 @@ public class App {
     private static final List<Account>  accounts = new ArrayList<>();
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Digite seu email: ");
-        String email = sc.nextLine();
-        System.out.println("Digite sua senha: ");
-        String password = sc.nextLine();
-        String result = App.createAccount(email, password);
-        System.out.println(result);
+        String option = "OPTION";
+        while(!option.equals("SAIR")){
+            System.out.println("Escolha sua opção: \nA: Criar conta \nB: Fazer login \nSAIR");
+            option = sc.nextLine();
+            String email, password, result = null;
+            switch (option) {
+                case "A":
+                    System.out.println("Digite seu email: ");
+                    email = sc.nextLine();
+                    System.out.println("Digite sua senha: ");
+                    password = sc.nextLine();
+                    result = App.createAccount(email, password);
+                    System.out.println(result);
+                    break;
+                case "B":
+                    System.out.println("Digite seu email: ");
+                    email = sc.nextLine();
+                    System.out.println("Digite sua senha: ");
+                    password = sc.nextLine();
+                    result = App.logIn(email, password);
+                    System.out.println(result);
+                    break;
+                default:
+                    break;
+            }
+    }
         sc.close();
     }
 
@@ -24,5 +44,17 @@ public class App {
         Account account = new Account(1L, email, passwordEncrypt);
         accounts.add(account);
         return "Account criada com sucesso!";
+    }
+
+    private static String logIn(String email, String password){
+        Account account = accounts.stream().filter(ac -> ac.email().equals(email)).findFirst().orElse(null);
+        if(account == null){
+            return "Conta não encontrada!";
+        }
+        String passwordDecrypted = Encrypty.decryptPassword(account.password());
+        if(!passwordDecrypted.equals(password)){
+            return "Falha no login!";
+        }
+        return "Login com sucesso!";
     }
 }
--- AuthenticationSystem/src/Encrypty.java
+++ AuthenticationSystem/src/Encrypty.java
@@ -5,17 +5,40 @@ import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
 
 public class Encrypty {
+    private static KeyGenerator keyGen ;
+    private static SecretKey secretKey ;
+    private static Cipher cipher ;
+
+    static {
+        try{
+            keyGen = KeyGenerator.getInstance("AES");
+            keyGen.init(256);
+            secretKey = keyGen.generateKey();
+    
+            cipher = Cipher.getInstance("AES");
+        } catch (Exception ex){
+            ex.printStackTrace();
+        }
+    }
+
 
     public static String encryptPassword(String password) {
+            byte[] encryptedPassword = new byte[]{};
+            try {
+                cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+                encryptedPassword = cipher.doFinal(password.getBytes());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return Base64.getEncoder().encodeToString(encryptedPassword);
+
+    }
+
+    public static String decryptPassword(String password){
         try {
-            KeyGenerator keyGen = KeyGenerator.getInstance("AES");
-            keyGen.init(256);
-            SecretKey secretKey = keyGen.generateKey();
-    
-            Cipher cipher = Cipher.getInstance("AES");
-            cipher.init(Cipher.ENCRYPT_MODE, secretKey);
-            byte[] encryptPassword = cipher.doFinal(password.getBytes());
-            return Base64.getEncoder().encodeToString(encryptPassword);
+            cipher.init(Cipher.DECRYPT_MODE, secretKey);
+            byte[] decryptedPassword = cipher.doFinal(Base64.getDecoder().decode(password));
+            return new String(decryptedPassword);
         } catch (Exception ex) {
             ex.printStackTrace();
         }
